a20cb8149e969568778cfcd24100bc10c9c98514
Yana <Yana.ilina.1999@list.ru>
2021-11-30 00:34:29 +0300

    ChangeBalance

---
+++ Card/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card
+{
+    class Class1
+    {
+        public double Balance { get; set; }
+        public string Name { get; set; }
+
+        public void ChangeBalance(double money)
+        {
+
+            Balance = Balance + money;
+        }
+    }
+}
---
+++ Card/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Card
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Balance.Text = Balance.Text + Summ.Text;
+        }
+
+        private void Balance_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            char lastChar = Balance.Text[Balance.Text.Length - 1];
+            if(!Char.IsDigit(lastChar) && lastChar!= ',')
+            {
+                Balance.Text = Balance.Text.Remove(Balance.Text.Length - 1);
+                Balance.CaretIndex = Balance.Text.Length;
+            }
+        }
+
+        private void Summ_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            char lastChar = Summ.Text[Summ.Text.Length - 1];
+            if (!Char.IsDigit(lastChar) && lastChar != ',')
+            {
+                Summ.Text = Summ.Text.Remove(Summ.Text.Length - 1);
+                Summ.CaretIndex = Summ.Text.Length;
+            }
+        }
+    }
+}
